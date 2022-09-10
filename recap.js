e23fa051205bf38a6f63f802e67c3468faccb416
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-10 07:01:00 +0600

    debag-fuctional-bank

---
+++ recap.js
@@ -0,0 +1,20 @@
+/* ........................jaaScript Consents.......................
+1. null vs undefined
+2. == vs ===
+3. scope:-> block, function, global
+4. closure
+5. hosting
+6. callback function
+7. argument. what is array like object.
+8. array vs array like object.
+9. truthy vs falsy.
+10. ! vs !1
+*/
+
+/* 
+1. Element tool
+2. console tool
+3. source tool
+4. Network tool
+5. Application tool
+ */
