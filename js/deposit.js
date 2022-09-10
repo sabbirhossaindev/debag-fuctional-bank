0792038eaba42e812c84b5ddb031bc50d52c6128
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-10 10:10:58 +0600

    diposit contorl

--- js/deposit.js
+++ js/deposit.js
@@ -7,19 +7,18 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
     */
   const newDepositAmount = getInputFieldValueById("deposit-field");
   /* 
-    1. get previous deposit total by id
-
+    1. get previous deposit total by id..
     */
   const previousDepositTotal = getTextElementValueById("deposit-total");
 
   // calculate new deposit total
   const newDepositTotal = previousDepositTotal + newDepositAmount;
-  console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
+  // console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
   // set deposit total value
   setTextElementValueById("deposit-total", newDepositTotal);
 
-  // get previous balance by using the function
-  const previousBalanceTotal = getextElementValueById("balance-total");
+  // get previous balance by using the function...
+  const previousBalanceTotal = getTextElementValueById("balance-total");
   const newBalanceTotal = previousBalanceTotal + newDepositAmount;
   setTextElementValueById("balance-total", newBalanceTotal);
-);
+});
--- js/utilities.js
+++ js/utilities.js
@@ -2,8 +2,16 @@ function getInputFieldValueById(inputFieldId) {
   const inputField = document.getElementById(inputFieldId);
   const inputFieldValueString = inputField.value;
   const inputFieldValue = parseFloat(inputFieldValueString);
+  inputField.value = '';
   return inputFieldValue;
-  inputField.value = "";
+}
+
+
+function getElementValueById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValueString = element.innerText;
+  const value = parseFloat(elementValueString)
+  return value;
 }
 
 function getTextElementValueById(elementId) {
